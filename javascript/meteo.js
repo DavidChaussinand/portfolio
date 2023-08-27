60514543349710b93601b4c1e69213869c3e69a9
david <lebib07@live.fr>
2023-08-27 10:49:31 +0200

    rajout d'une api météo sur le footer de la page d'accueil

---
+++ javascript/meteo.js
@@ -0,0 +1,40 @@
+const errorInformation = document.querySelector(".error-information");
+
+async function getWeatherData(){
+  try {
+    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=5dfff0f5-926e-4a53-96a7-4b3b615a1f32")
+
+    if(!response.ok) {
+      throw new Error(`Error ${response.status}, ${response.statusText}`)
+    }
+
+    const responseData = await response.json();
+    
+    const sortedData = {
+      city: responseData.data.city,
+      country: responseData.data.country,
+      iconId: responseData.data.current.weather.ic,
+      temperature: responseData.data.current.weather.tp,
+    }
+
+    populateUI(sortedData)
+  }
+  catch (error) {
+    errorInformation.textContent = error.message;
+  }
+}
+
+
+const cityName = document.querySelector(".city-name");
+const temperature = document.querySelector(".temperature");
+const infoIcon = document.querySelector(".info-icon");
+
+function populateUI(data){
+  cityName.textContent = data.city;
+  temperature.textContent = `${data.temperature}°`;
+  infoIcon.src = `ressource/meteo/icons/${data.iconId}.svg`;
+  infoIcon.style.width = "50px";
+}
+
+
+getWeatherData()
